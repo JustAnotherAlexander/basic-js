528a53f677a38665089760455b99b0c6f73910a7
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-29 23:18:47 +0300

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+let string = '';
+
+let repeatTimes = options.repeatTimes;  
+let separator = options.separator;
+let addition = options.addition ;
+let additionRepeatTimes = options.additionRepeatTimes;
+let additionSeparator = options.additionSeparator;
+
+if (additionRepeatTimes == undefined) {
+  additionRepeatTimes = 1;
+}
+
+if (repeatTimes == undefined) {
+  repeatTimes = 1;
+}
+
+if (separator == undefined) {
+  separator = '+';
+}
+
+if (additionSeparator == undefined) {
+  additionSeparator = '';
+} else if (additionSeparator==null) {
+  additionSeparator = 'null';
+}
+
+ addition = addition == null && options.additionRepeatTimes != undefined?'null': addition == undefined?'':addition.toString();
+
+ str = str == null?'null':str.toString();
+
+for(let i = 0; i < repeatTimes; i++){
+  string += str;
+  for(let j = 0; j < additionRepeatTimes ; j++){
+    string += addition;
+    if(additionRepeatTimes>1 && j<additionRepeatTimes-1){
+      string += additionSeparator;
+    }
+  }
+ if(repeatTimes>1 && i<repeatTimes-1){
+    string += separator;
+  } 
+}
+return string;
 };
-  
