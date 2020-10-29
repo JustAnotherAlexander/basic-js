38809a7f74d994b2117ef7b8db99d7d72b340b92
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-29 20:52:22 +0300

    feat: add solution to cats and carbon-dating tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,26 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity ) {
+
+if (typeof(sampleActivity)!='string') {
+  return false;
+}
+sampleActivity=sampleActivity.split('');
+
+for (let i=0; i<sampleActivity.length; i++) {
+  if ((sampleActivity[i].match(/[a-zA-Z]/))!=null) {
+    return false;
+  }
+}
+if (sampleActivity.indexOf('.')!=-1) {
+  let index=sampleActivity.indexOf('.');
+  sampleActivity.splice(index,1);
+}
+if (+sampleActivity > MODERN_ACTIVITY || isNaN(+sampleActivity) || +sampleActivity<=0) {
+  return false;
+}
+sampleActivity.join('');
+ return Math.floor(HALF_LIFE_PERIOD / 0.693 * Math.log(sampleActivity/MODERN_ACTIVITY) * -1 )+1;
+
 };
