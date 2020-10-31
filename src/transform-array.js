2ff45ab833b4c079af3acf3e8c8c0b328770356e
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-31 15:22:35 +0300

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  let mods = ['--discard-next','--discard-prev','--double-next','--double-prev'];
+  let array = arr.slice(0, arr.length);
+  let result = [];
+  
+  if(!Array.isArray(array))   {
+       throw new Error();  
+      }
+  if(array.length == 0)       { 
+      return [];                     
+    }
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  for(let i = 0 ; i < array.length ; i++ ){
+      switch (array[i]) {
+          case '--discard-next':
+              if(!(i == array.length-1 || mods.includes(array[i+1]))) { 
+                 array[i+1] = null; 
+                }
+              break;
+          case '--discard-prev':
+              if(!(i == 0 || mods.includes(array[i-1])))           {
+                  array[i-1] = null; 
+                }
+              break;
+
+          case '--double-next':
+              if(!(i == array.length-1 || mods.includes(array[i+1]))) { 
+                 array.splice(i, 1, array[i + 1]); 
+                 }
+              break;
+
+          case '--double-prev':
+              if(!(i == 0 || mods.includes(array[i-1]))) {
+                   array.splice(i, 1, array[i - 1]);
+                  } else {
+                       array.splice(i, 1);           
+                       i--;  
+                      }
+              break;
+
+          default: break;
+      }
+  }
+
+  for(let i = 0 ; i < array.length ; i++ ){
+     if(array[i] != '--discard-next' && array[i] != '--discard-prev' && 
+        array[i] != '--double-next' && array[i] != '--double-prev' && array[i] != null)
+      {
+          result.push(array[i]);
+      }
+  }
+  return result;
 };
